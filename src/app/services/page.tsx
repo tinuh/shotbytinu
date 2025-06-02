699ce2f97ca6ebc95280963a99c7c82330406f77
Tinu Vanapamula <tinuvanapamula@gmail.com>
2025-06-02 01:40:54 -0400

    add padding for services to fix issue on mobile

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
 	return (
 		<div>
 			<h1 className="pt-5 text-4xl text-center">Services</h1>
-			<div className="px-5 lg:px-30 pt-4 no-serif flex justify-center">
+			<div className="px-5 lg:px-30 pb-10 pt-4 no-serif flex justify-center">
 				<div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
 					<Card
 						name="Potraits"
